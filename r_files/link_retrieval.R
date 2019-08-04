5f395360713d010884ce10b4ab9c5c983a3314df
Megan Stodel <megjcs@gmail.com>
2019-08-04 16:02:57 +0100

    Refine case links retrieval

--- r_files/link_retrieval.R
+++ r_files/link_retrieval.R
@@ -1,17 +1,20 @@
 library(rvest)
+library(data.table)
 
 #### Create vector of links for scraper to use
 
+case_links <- character(0)
+
 # NB - the below takes several minutes - recommend using the case_links.feather file that exists
 
-for (page_num in 1:933) {
+for (page_num in 1:938) {
 
   # Link for each new page
   page <- xml2::read_html(paste0("https://tribunalsdecisions.service.gov.uk/utiac?&page=",
                            page_num))
   # Get links of cases from page
   new_links <- page %>%
-    html_nodes(".unreported a") %>%
+    html_nodes("td:nth-child(1) a") %>%
     html_attr('href')
 
 
